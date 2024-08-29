f03c3c69231e852e56d5be151ac1e93e40fb7bf4
ilyeshr2 <128542440+ilyeshr2@users.noreply.github.com>
2024-08-28 21:20:14 -0400

    carShow sceen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import './index.css';
+import { OrbitControls, PerspectiveCamera, SpotLight} from '@react-three/drei';
+import { Ground } from './components/Ground';
+
+function CarShow() {
+  return (
+    <>
+      <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
+
+      <PerspectiveCamera makeDefault fov={100} position={[3, 2, 5]} />
+
+      <color attach="background" args={[0,0,0]} />
+
+      <SpotLight
+        color={[1,0.25,0.7]}
+        intensity={1.5}
+        angle={0.5} 
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}  
+        />
+      <SpotLight
+        color={[0.14,0.5,1]}
+        intensity={2}
+        angle={0.6} 
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}  
+        />
+      <Ground />
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={null}>
+      <Canvas>
+          <CarShow />
+      </Canvas>
+    </Suspense>
   );
 }
 
