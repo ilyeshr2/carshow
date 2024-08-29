a7d6a1589c0c0392e73b476b93487bd2cf8e7c76
ilyeshr2 <128542440+ilyeshr2@users.noreply.github.com>
2024-08-29 17:28:44 -0400

    adding a car model to the sceen

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import './index.css';
 import { OrbitControls, PerspectiveCamera} from '@react-three/drei';
 import { Ground } from "./Ground";
+import { Car } from './Car';
 
 function CarShow() {
   return (
@@ -13,14 +14,15 @@ function CarShow() {
       />
 
       <PerspectiveCamera 
-        makeDefault fov={100} 
-        position={[3, 2, 5]} 
+        makeDefault 
+        fov={50} 
+        position={[6, 1.5, 3]} 
       />
 
       <color attach="background" args={[0,0,0]} />
-
+      <Car />
       <spotLight
-        color={[1, 1, 1]}
+        color={[1, 0.25, 0.7]}
         intensity={1.5}
         angle={0.6}
         penumbra={0.5}
@@ -29,13 +31,13 @@ function CarShow() {
         shadow-bias={-0.0001}
       />
       <spotLight
-        color={[1, 1, 1]}
+        color={[0.14, 0.5, 1]}
         intensity={2}
-        angle={0.6} 
+        angle={0.6}
         penumbra={0.5}
         position={[-5, 5, 0]}
         castShadow
-        shadow-bias={-0.0001}  
+        shadow-bias={-0.0001}
       />
       <Ground />
     </>
