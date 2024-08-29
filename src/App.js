05e7233951c72a7813ed5d5a1dd7a3ff53753e77
ilyeshr2 <ilyesharrache0@gmail.com>
2024-08-29 06:36:39 +0100

    .

--- src/App.js
+++ src/App.js
@@ -19,24 +19,24 @@ function CarShow() {
 
       <color attach="background" args={[0,0,0]} />
 
-      <SpotLight
-        color={[1,0.25,0.7]}
+      <spotLight
+        color={[1, 0.25, 0.7]}
         intensity={1.5}
-        angle={0.5} 
+        angle={0.6}
         penumbra={0.5}
         position={[5, 5, 0]}
         castShadow
-        shadow-bias={-0.0001}  
-        />
+        shadow-bias={-0.0001}
+      />
       <SpotLight
-        color={[0.14,0.5,1]}
+        color={[0.14, 0.5, 1]}
         intensity={2}
         angle={0.6} 
         penumbra={0.5}
         position={[-5, 5, 0]}
         castShadow
         shadow-bias={-0.0001}  
-        />
+      />
       <Ground />
     </>
   );
--- src/Ground.js
+++ src/Ground.js
@@ -3,9 +3,8 @@ import { useFrame, useLoader } from "@react-three/fiber";
 import { MeshReflectorMaterial } from "@react-three/drei";
 import { LinearEncoding, RepeatWrapping, TextureLoader } from "three";
 
-
 export function Ground() {
-
+  // thanks to https://polyhaven.com/a/rough_plasterbrick_05 !
   const [roughness, normal] = useLoader(TextureLoader, [
     process.env.PUBLIC_URL + "textures/terrain-roughness.jpg",
     process.env.PUBLIC_URL + "textures/terrain-normal.jpg",
