4ae95e6d5ae4239ff2b3319936023734d3870926
ilyeshr2 <128542440+ilyeshr2@users.noreply.github.com>
2024-08-29 20:20:04 -0400

    .

--- src/Car.js
+++ src/Car.js
@@ -1,16 +1,17 @@
 import React, { useEffect } from "react";
 import { useFrame, useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { Mesh } from "three";
+import { TextureLoader, Mesh } from "three";
 
-// based on "Chevrolet Corvette (C7)" (https://sketchfab.com/3d-models/chevrolet-corvette-c7-2b509d1bce104224b147c81757f6f43a) 
-// by Martin Trafas (https://sketchfab.com/Bexxie) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
 export function Car() {
   const gltf = useLoader(
     GLTFLoader,
     process.env.PUBLIC_URL + "models/car/scene.gltf"
   );
-  
+
+  // Load your texture (replace with your actual image path)
+  const bodyTexture = useLoader(TextureLoader, process.env.PUBLIC_URL + "/models/car/textures/TireMaterial_baseColor.png");
+
   useEffect(() => {
     gltf.scene.scale.set(0.005, 0.005, 0.005);
     gltf.scene.position.set(0, -0.035, 0);
@@ -18,12 +19,15 @@ export function Car() {
       if (object instanceof Mesh) {
         object.castShadow = true;
         object.receiveShadow = true;
-        object.material.envMapIntensity = 20;
+
+        object.material.map = bodyTexture;
+        object.material.needsUpdate = true;
+         
       }
     });
-  }, [gltf]);
+  }, [gltf, bodyTexture]);
 
-  useFrame((state, delta) => {
+  useFrame((state) => {
     let t = state.clock.getElapsedTime();
 
     let group = gltf.scene.children[0].children[0].children[0];
